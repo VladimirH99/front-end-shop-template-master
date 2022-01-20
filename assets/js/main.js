fea5eabc82b8e27be46d78f77676cca48fc805c1
Vladimir Homich <homichvova91@gmail.com>
2022-01-20 21:10:54 +0300

    выполнена основаная часть задания с преподавателем

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
+
 const contentContainer = document.querySelector('#content-container');
 const cartCounterLabel = document.querySelector('#cart-counter-label');
-
 let cartCounter = 0;
 let cartPrice = 0;
 
@@ -12,43 +12,39 @@ const incrementCounter = () => {
 
 const getMockData = (t) => +t.parentElement
   .previousElementSibling
-  .innerHTML
-  .replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
+  .innerHTML.replace(/^\$(\d+)\s\D+(\d+).*$/, '$1.$2');
 
 const getPrice = (t, price) => Math.round((price + getMockData(t)) * 100) / 100;
-
 const disableControls = (t, fn) => {
-  contentContainer.removeEventListener('click', fn);
   t.disabled = true;
-};
-
+  contentContainer.removeEventListener('click', fn);
+}
 const enableControls = (t, fn) => {
-  contentContainer.addEventListener('click', fn);
   t.disabled = false;
-};
+  contentContainer.addEventListener('click', fn);
+}
 
 const btnClickHandler = (e) => {
   const target = e.target;
   const interval = 2000;
-
   let restoreHTML = null;
 
   if (target && target.matches('.item-actions__cart')) {
-
     incrementCounter();
 
     cartPrice = getPrice(target, cartPrice);
     restoreHTML = target.innerHTML;
-
     target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
-
     disableControls(target, btnClickHandler);
 
     setTimeout(() => {
-      target.innerHTML = restoreHTML;
+      target.innerHTML = restoreHTML
       enableControls(target, btnClickHandler);
-    }, interval);
+    }, interval)
+
+    console.log(cartPrice);
   }
 };
 
 contentContainer.addEventListener('click', btnClickHandler);
+
